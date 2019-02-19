e33cda44f6ee6dcae28ee34f3db282ea82faf13c
That Lisa <thatlisajones@gmail.com>
2019-02-19 14:01:30 -0700

    Refactor

--- models/burger.js
+++ models/burger.js
@@ -1,24 +1,24 @@
-var orm = require("../config/orm.js");
+// Burger models
 
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  create: function(name, cb) {
-    orm.create("burgers", [
-      "burger_name", "devoured"
-    ], [
-      name, false
-    ], cb);
-  },
-  update: function(id, cb) {
-    var condition = "id=" + id;
-    orm.update("burgers", {
-      devoured: true
-    }, condition, cb);
-  }
+// The burger has a burger_name attribute of type DataTypes.String
+// and a devoured attribute that is false by default
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Burger.hasOne(models.Customer);
+      }
+    }
+  });
+  return Burger;
 };
-
-module.exports = burger;
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
-    
+
   $(".devour-form").on("submit", function(event) {
     event.preventDefault();
 
-    var burger_id = $(this).children(".burger_id").val();
-    console.log(burger_id);
+    var burgerInfo = {
+      burger_id: $(this).children(".burger_id").val(),
+      customer: $(this).children(".custom-input").val()
+    };
+
     $.ajax({
       method: "PUT",
-      url: "/burgers/" + burger_id
+      url: "/burgers/update",
+      data: burgerInfo
     }).then(function(data) {
       // reload page to display devoured burger in proper column
       location.reload();
